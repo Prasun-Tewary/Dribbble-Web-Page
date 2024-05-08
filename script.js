221bf4a37e84e69e569c1aa0881baa6b5c5bcfeb
PRASUN TEWARY <158489958+Prasun-Tewary@users.noreply.github.com>
2024-05-08 20:06:05 +0530

    Upload Successful

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.querySelector("#click").addEventListener("click",function(){
+  window.open("https://dribbble.com/shots/popular", "_blank");
+});
+
+document.querySelector(".click2").addEventListener("click",function(){
+  window.open("https://dribbble.com/signup/new", "_blank");
+});
+document.querySelector(".click3").addEventListener("click",function(){
+  window.open("https://dribbble.com/signup/new", "_blank");
+});
+document.querySelector("#click4").addEventListener("click",function(){
+  window.open("https://dribbble.com/hiring", "_blank");
+});
+
